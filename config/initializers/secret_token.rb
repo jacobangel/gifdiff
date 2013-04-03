feec4e20e4f44789232f11f9da2830857997aae1
Jacob Angel <jacob.angel@gmail.com>
2013-04-03 17:44:27 -0400

    Initial commit
    This is basically just a rails default with a custom gitignore.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gifdiff::Application.config.secret_token = '647e456326f1d4d180e46e2ee0fe04e890519d0e657bd0bf043e4c9b0b4deb94df973efecf4c4a22a6dc7b191e06d484c25a1037001d8ee26665f748d5908771'
